2a748f460098f5cdacc7b8f3965a0707d0b3a4b3
JamesWithCode <minseok.jeong.james@gmail.com>
2022-01-25 22:48:00 +0900

    frontend 뷰 설정

---
+++ src/main/java/com/jamesdev/springbootinstagramclone/controller/testController.java
@@ -0,0 +1,45 @@
+package com.jamesdev.springbootinstagramclone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+
+      @GetMapping("/join")
+      public String test(){
+            return "auth/join";
+      }
+      @GetMapping("/loginn")
+      public String test2(){
+            return "auth/login";
+      }
+
+      @GetMapping("/explore")
+      public String test3(){
+            return "post/explore";
+      }
+
+      @GetMapping("/story")
+      public String test4(){
+            return "post/story";
+      }
+
+      @GetMapping("/upload")
+      public String test5(){
+            return "post/upload";
+      }
+
+      @GetMapping("/profile")
+      public String test6(){
+            return "user/profile";
+      }
+
+      @GetMapping("/update")
+      public String test7(){
+            return "user/update";
+      }
+
+
+
+}
