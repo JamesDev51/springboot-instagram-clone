5de74912d2ad16398789d3155ecdc86a81134583
JamesWithCode <minseok.jeong.james@gmail.com>
2022-01-25 20:46:56 +0900

    Initial commit

---
+++ src/main/java/com/jamesdev/springbootinstagramclone/SpringbootInstagramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.jamesdev.springbootinstagramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInstagramCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInstagramCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamesdev/springbootinstagramclone/SpringbootInstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesdev.springbootinstagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInstagramCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
