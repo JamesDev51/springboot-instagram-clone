5c20f00ece19ab436efbe4ae5b837581fa876cf6
JamesWithCode <minseok.jeong.james@gmail.com>
2022-01-30 21:07:56 +0900

    회원수정 api완료

--- src/main/java/com/jamesdev/springbootinstagramclone/controller/api/UserApiController.java
+++ src/main/java/com/jamesdev/springbootinstagramclone/controller/api/UserApiController.java
@@ -1,13 +1,18 @@
 package com.jamesdev.springbootinstagramclone.controller.api;
 
+import com.jamesdev.springbootinstagramclone.config.auth.PrincipalDetails;
+import com.jamesdev.springbootinstagramclone.domain.user.User;
 import com.jamesdev.springbootinstagramclone.dto.ResponseDto;
 import com.jamesdev.springbootinstagramclone.dto.auth.EmailDupCheckDto;
 import com.jamesdev.springbootinstagramclone.dto.auth.JoinDto;
 import com.jamesdev.springbootinstagramclone.dto.auth.UsernameDupCheckDto;
+import com.jamesdev.springbootinstagramclone.dto.user.UserUpdateDto;
 import com.jamesdev.springbootinstagramclone.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +38,14 @@ public class UserApiController {
             if(userService.signUp(joinDto)) return new ResponseEntity<>(new ResponseDto<>("회원가입이 완료되었습니다."),HttpStatus.CREATED);
             return new ResponseEntity<>(new ResponseDto<>("회원가입에 실패하였습니다."),HttpStatus.BAD_REQUEST);
       }
+
+      @PostMapping("/api/user/{id}")
+      public ResponseEntity<?> editUser(@PathVariable int id,
+                                        @RequestBody UserUpdateDto userUpdateDto,
+                                        @AuthenticationPrincipal PrincipalDetails principalDetails){
+            User userEntity = userService.editUser(id,userUpdateDto);
+            principalDetails.setUser(userEntity);
+            return new ResponseEntity<>(new ResponseDto<>(userEntity),HttpStatus.OK);
+      }
+
 }
--- src/main/java/com/jamesdev/springbootinstagramclone/service/UserService.java
+++ src/main/java/com/jamesdev/springbootinstagramclone/service/UserService.java
@@ -6,9 +6,12 @@ import com.jamesdev.springbootinstagramclone.domain.user.UserRepository;
 import com.jamesdev.springbootinstagramclone.dto.auth.EmailDupCheckDto;
 import com.jamesdev.springbootinstagramclone.dto.auth.JoinDto;
 import com.jamesdev.springbootinstagramclone.dto.auth.UsernameDupCheckDto;
+import com.jamesdev.springbootinstagramclone.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
@@ -19,11 +22,13 @@ public class UserService {
       private final UserRepository userRepository;
       private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+      @Transactional(readOnly = true)
       public boolean isUsernameAvailable(UsernameDupCheckDto usernameDupCheckDto){
             String username= usernameDupCheckDto.getUsername();
             return userRepository.findByUsername(username).isPresent();
       }
 
+      @Transactional(readOnly = true)
       public boolean isEmailAvailable(EmailDupCheckDto emailDupCheckDto) {
             String email= emailDupCheckDto.getEmail();
             return userRepository.findByEmail(email).isPresent();
@@ -33,6 +38,7 @@ public class UserService {
             return Objects.equals(joinDto.getPassword1(), joinDto.getPassword2());
       }
 
+      @Transactional
       public boolean signUp(JoinDto joinDto){
             //TODO: 권한설정 (USER, ADMIN) 지금은 그냥 USER
             if (!isPasswordEqual(joinDto)) return false;
@@ -43,5 +49,21 @@ public class UserService {
             userRepository.save(newUser);
             return true;
 
+      }
+
+
+      @Transactional
+      public User editUser(int id, UserUpdateDto userUpdateDto) {
+            User userEntity = userRepository.findById(id).orElseThrow(()-> new UsernameNotFoundException("찾을 수 없음"));
+
+            String rawPassword = userUpdateDto.getPassword();
+            String encPassword= Objects.equals(rawPassword, "") ?userEntity.getPassword():bCryptPasswordEncoder.encode(rawPassword); //비었으면 원래 비밀번호, 있으면 인코딩
+
+            userEntity.setPassword(encPassword);
+            userEntity.setName(userUpdateDto.getName());
+            userEntity.setBio(userUpdateDto.getBio());
+            userEntity.setWebsite(userUpdateDto.getWebsite());
+            userEntity.setPhone(userUpdateDto.getPhone());
+            return userEntity;
       }
 }
