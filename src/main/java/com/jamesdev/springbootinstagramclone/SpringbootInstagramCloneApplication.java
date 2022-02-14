5e6005fd8e05c36b3a433b4d326a7bcf0ec479f0
JamesWithCode <minseok.jeong.james@gmail.com>
2022-02-14 20:49:06 +0900

    aws s3

--- src/main/java/com/jamesdev/springbootinstagramclone/SpringbootInstagramCloneApplication.java
+++ src/main/java/com/jamesdev/springbootinstagramclone/SpringbootInstagramCloneApplication.java
@@ -2,12 +2,19 @@ package com.jamesdev.springbootinstagramclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootInstagramCloneApplication {
 
+    public static final String APPLICATION_LOCATIONS="spring.config.location="
+                +"classpath:application.yml ,"
+                +"classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootInstagramCloneApplication.class, args);
+        new SpringApplicationBuilder(SpringbootInstagramCloneApplication.class)
+                    .properties(APPLICATION_LOCATIONS)
+                    .run(args);
     }
 
 }
---
+++ src/main/java/com/jamesdev/springbootinstagramclone/controller/S3TestController.java
@@ -0,0 +1,24 @@
+package com.jamesdev.springbootinstagramclone.controller;
+
+import com.jamesdev.springbootinstagramclone.service.AmazonS3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class S3TestController {
+
+      private final AmazonS3Uploader s3Uploader;
+
+      @PostMapping("/images")
+      public String upload(@RequestParam("images")MultipartFile multipartFile)throws IOException {
+            s3Uploader.upload(multipartFile,"static");
+            return "test";
+      }
+
+}
